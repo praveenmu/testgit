17dcbd7f83979096bbd72ab2c1a57ecf2a5c9c5b
Praveen Muthusamy <praveen.gct@gmail.com>
2013-12-29 18:09:16 +0530

    test commit

---
+++ src/Solution.java
@@ -0,0 +1,16 @@
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		testExcThrows();
+	}
+	
+	private static void testExcThrows()
+	{
+		System.out.println("test exc throws");
+		throw new Error();
+	
+	}
+
+}
