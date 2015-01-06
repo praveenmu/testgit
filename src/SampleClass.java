81915929f72a5a33132089ccf186bc0da1c0916c
praveen.muthusamy <praveenmuthusamy@flipkarts-MacBook-Pro.local>
2015-01-06 17:44:00 +0530

    conflict

--- src/SampleClass.java
+++ src/SampleClass.java
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
 public class SampleClass {
 
 	public void add(int a, int b) {
 		int c = a + b;
-		System.out.println(c + "123test");
-	}
-	
-	public void testGitEdit(int y)
-	{
-		System.out.println(y);
-	}
-}
-=======
-public class SampleClass {
-	public int add(int a, int b) {
-		int c = a + b;
-
-		return c;
 		System.out.println(c + "test");
 	}
 	
@@ -25,4 +10,3 @@ public class SampleClass {
 		System.out.println(y);
 	}
 }
->>>>>>> 94b6542b33313cb205e0bc62960f8f5634ac49ef
