94b6542b33313cb205e0bc62960f8f5634ac49ef
saravanan.arumugam <saravanan.arumugam@flipkart.com>
2015-01-06 17:22:08 +0530

    fixed the broken merge

--- src/SampleClass.java
+++ src/SampleClass.java
@@ -1,12 +1,9 @@
 public class SampleClass {
-
 	public int add(int a, int b) {
 		int c = a + b;
-<<<<<<< HEAD
+
 		return c;
-=======
 		System.out.println(c + "test");
->>>>>>> f99f4c6e8e8a539d00290ecc5572461ec2707066
 	}
 	
 	public void testGitEdit(int y)
