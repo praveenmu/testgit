5637b70cbe4844cea23ad88cf57ca4c0ebbfcb35
Praveen Muthusamy <praveen.muthusamy@flipkart.com>
2018-01-24 12:29:51 +0530

    Test change in a branch

--- src/SampleClass.java
+++ src/SampleClass.java
@@ -1,7 +1,7 @@
 public class SampleClass {
 	public int add(int a, int b) {
 		int c = a + b;
-
+		System.out.println("testing edit in a new branch");
 		return c;
 		System.out.println(c + "test");
 	}
